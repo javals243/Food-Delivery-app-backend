7d5525d5f78f5cdb413d6edb94ac6e9764f341f1
javals243 <vchihisa@gmail.com>
2022-10-09 15:18:29 +0200

    initial app

---
+++ dist/config/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PORT = exports.APP_SECRET = exports.MONGO_URI = void 0;
+exports.MONGO_URI = 'mongodb://localhost:27017/online_food_delivery';
+exports.APP_SECRET = '238745623hsdf';
+exports.PORT = process.env.PORT || 8000;
+//# sourceMappingURL=index.js.map
